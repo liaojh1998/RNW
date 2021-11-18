354810a6987af6aca158deef9680ca9114bcde2c
Jie Hao Liao <liaojh1998@gmail.com>
2021-11-18 02:47:10 -0600

    Added environment setup

---
+++ env/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+source ~/.bashrc
+
+#conda env create -f environment.yml
+conda activate rnw
+pip3 install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
